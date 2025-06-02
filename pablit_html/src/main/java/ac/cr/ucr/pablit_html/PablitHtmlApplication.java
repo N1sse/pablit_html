3c7315d60b717aa7a6556d02944d7c66a2ec4fed
N1sse <ramirezdenisse01@gmail.com>
2025-06-02 15:48:44 -0600

    primer commit

---
+++ pablit_html/src/main/java/ac/cr/ucr/pablit_html/PablitHtmlApplication.java
@@ -0,0 +1,13 @@
+package ac.cr.ucr.pablit_html;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PablitHtmlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PablitHtmlApplication.class, args);
+	}
+
+}
---
+++ pablit_html/src/test/java/ac/cr/ucr/pablit_html/PablitHtmlApplicationTests.java
@@ -0,0 +1,13 @@
+package ac.cr.ucr.pablit_html;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PablitHtmlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
