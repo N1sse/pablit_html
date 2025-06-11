4173090d24528537324b2338e57d501399cf6904
PabloGCalderon <pabloandresgc8@gmail.com>
2025-06-11 17:13:01 -0600

    Se creo la tabla User para almacenar datos del usuario, el modulo usuario en este caso registra los datos ingresados como username,password,age,height y peso. Maneja un id autoincremental por lo que el usuario no debe de escribir un id al registrarse. El controller busca un usuario por username ademas de permitir hacer post de nuevos usuarios y modificar usuarios. Todo esto es realizado con una base de datos remota

--- pablit_html/src/main/java/ac/cr/ucr/pablit_html/controller/UserController.java
+++ pablit_html/src/main/java/ac/cr/ucr/pablit_html/controller/UserController.java
@@ -1,4 +1,87 @@
 package ac.cr.ucr.pablit_html.controller;
 
-public class UserController {
+import ac.cr.ucr.pablit_html.model.User;
+import ac.cr.ucr.pablit_html.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("api/users")
+public class UserController
+{
+    @Autowired
+    UserService userService;
+
+
+    @GetMapping("/FindUsername/{username}")
+    public ResponseEntity<?> getUser(@PathVariable String username)
+    {
+        Optional<User> user =  this.userService.findByUsername(username);
+        if(!user.isPresent())
+        {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("El usuario con el usuario " + username + " no fue encontrado");
+        }
+        return ResponseEntity.ok(user);
+    }
+
+    @PostMapping
+    public ResponseEntity<?> addUser(@Validated @RequestBody User user, BindingResult result)
+    {
+        if(result.hasErrors())
+        {
+            Map<String, String> errors = new HashMap<>();
+            for(FieldError error: result.getFieldErrors())
+            {
+                errors.put(error.getField(),error.getDefaultMessage());
+            }
+            return ResponseEntity.badRequest().body(errors);
+        }
+
+        Optional<User> userOp=this.userService.findByUsername(user.getUsername());
+        if(userOp.isPresent())
+        {
+            return ResponseEntity.status(HttpStatus.CONFLICT).body("El username "+ user.getUsername()+" ya se encuentra en uso");
+        }
+
+        User userAdd=this.userService.saveUser(user);
+        return ResponseEntity.status(HttpStatus.CREATED).body(userAdd);
+    }
+
+    @PutMapping("/editByID/{id}")
+    public ResponseEntity<?> editUser(@Validated @PathVariable Integer id,@RequestBody User userEdit, BindingResult result)
+    {
+        if(result.hasErrors())
+        {
+            Map<String, String> errors = new HashMap<>();
+            for(FieldError error: result.getFieldErrors())
+            {
+                errors.put(error.getField(),error.getDefaultMessage());
+            }
+            return ResponseEntity.badRequest().body(errors);
+        }
+
+        Optional<User> userOp=this.userService.findByIDUser(id);
+        if(userOp.isPresent())
+        {
+            if(id!=userEdit.getId())
+            {
+                return ResponseEntity.status(HttpStatus.CONFLICT).body("EL id de busqueda no es igual al ID del objeto");
+            }
+            else
+            {
+                return ResponseEntity.ok(this.userService.putUser(id, userEdit));
+            }
+        }
+        return ResponseEntity.status(HttpStatus.CONFLICT).body("El usuario con el ID "+id+" no se encuentra registrado");
+    }
 }
--- pablit_html/src/main/java/ac/cr/ucr/pablit_html/repository/UserRepository.java
+++ pablit_html/src/main/java/ac/cr/ucr/pablit_html/repository/UserRepository.java
@@ -1,4 +1,13 @@
 package ac.cr.ucr.pablit_html.repository;
 
-public interface UserRepository {
+import ac.cr.ucr.pablit_html.model.User;
+import jakarta.persistence.criteria.CriteriaBuilder;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Integer>
+{
+    Optional<User> findByUsername(String username);
+
 }
